c2e261084abb07fcbea27ab892a02c54d3169676
felgharb <faycal86@gmail.com>
2015-08-04 10:20:56 +0200

    amélioration de la timeline des utilisateurs (front et photographes)

--- gestion/src/Main/CommonBundle/Form/Services/FormPrestationType.php
+++ gestion/src/Main/CommonBundle/Form/Services/FormPrestationType.php
@@ -54,16 +54,18 @@ class FormPrestationType extends AbstractType
                         new NotBlank ( array(
                         )))
             ))
-            ->add('time', 'time', array(
-                'label' => 'form.prestation.field.time',
+            ->add('time', 'choice', array(
+                'label' => false,
+                'empty_value' => 'form.search.empty.startTime',
+                'choices'  => $this->getTimeChoices(),
+                'data'   => $date->format('h:i'),
                 'attr' => array(
-                        'class' => 'form-control',
-                ),
-                'data'   => $date,
-                'widget' => 'single_text',
+                        'class' => 'form-control selecter_3',
+                        ),
                 'constraints'   => array(
-                        new NotBlank ( array()))
-            ));
+                        new NotBlank ( array()),
+                        new Choice(array('choices' => array_keys($this->getTimeChoices()))))
+        ));
             
         }elseif ($slug == 'duration') {
             $devis = $options['devis'];
@@ -118,6 +120,59 @@ class FormPrestationType extends AbstractType
                 'price'              => null,
                 'duration'           => null
     	));
+    }
+
+    private function getTimeChoices(){
+        return array(
+            '00:00' => '00:00',
+            '00:30' => '00:30',
+            '01:00' => '01:00',
+            '01:30' => '01:30',
+            '02:00' => '02:00',
+            '02:30' => '02:30',
+            '03:00' => '03:00',
+            '03:30' => '03:30',
+            '04:00' => '04:00',
+            '04:30' => '04:30',
+            '05:00' => '05:00',
+            '05:30' => '05:30',
+            '06:00' => '06:00',
+            '06:30' => '06:30',
+            '07:00' => '07:00',
+            '07:30' => '07:30',
+            '08:00' => '08:00',
+            '08:30' => '08:30',
+            '09:00' => '09:00',
+            '09:30' => '09:30',
+            '10:00' => '10:00',
+            '10:30' => '10:30',
+            '11:00' => '11:00',
+            '11:30' => '11:30',
+            '12:00' => '12:00',
+            '12:30' => '12:30',
+            '13:00' => '13:00',
+            '13:30' => '13:30',
+            '14:00' => '14:00',
+            '14:30' => '14:30',
+            '15:00' => '15:00',
+            '15:30' => '15:30',
+            '16:00' => '16:00',
+            '16:30' => '16:30',
+            '17:00' => '17:00',
+            '17:30' => '17:30',
+            '18:00' => '18:00',
+            '18:30' => '18:30',
+            '19:00' => '19:00',
+            '19:30' => '19:30',
+            '20:00' => '20:00',
+            '20:30' => '20:30',
+            '21:00' => '21:00',
+            '21:30' => '21:30',
+            '22:00' => '22:00',
+            '22:30' => '22:30',
+            '23:00' => '23:00',
+            '23:30' => '23:30'
+            );
     }
 
     public function getName()
