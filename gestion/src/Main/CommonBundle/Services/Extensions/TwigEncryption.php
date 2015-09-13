eb486dfe2cf621c30dbb9e5f579b775d812746dc
felgharb <faycal86@gmail.com>
2015-09-13 15:14:55 +0200

    Préparation V0.9
    Début encryption des messages de presentation photographe et de
    presentation devis

---
+++ gestion/src/Main/CommonBundle/Services/Extensions/TwigEncryption.php
@@ -0,0 +1,49 @@
+<?php
+namespace Main\CommonBundle\Services\Extensions;
+
+use Symfony\Component\Finder\Finder;
+
+class TwigEncryption extends \Twig_Extension
+{
+
+    public function getFunctions()
+    {
+        return array(
+            'encryptText' => new \Twig_Function_Method($this, 'encryptText'),
+        );
+    }
+    
+    public function encryptText($text, $name)
+    {
+    	$final = mb_strtolower($text, 'UTF-8');
+    	$test = array('photographe', 'autodidacte', 'attente');
+    	$social = array(
+    		'facebook','twitter','instagram', 'flickr', '500px'
+    		); 
+    	$emails = array(
+    		'gmail', 'hotmail', 'yahoo', 'msn','free','outlook',
+    		'laposte','orange','wanadoo','netcourrier','net-c',
+    		'aol', 'sfr', 'gmx', 'voila','alice','bbox','live',
+    		'numericable'
+    		);
+    	$search = array('bing', 'google','altavista','baidu', 
+    		'yandex', 'duck', 'microsoft');
+    	$tld = array('.com','.fr','.net','.org','.es','.de',
+    		'.gouv','.co.uk','.co');
+    	$others = array('point','@');
+    	$final = str_replace($social,'***',$final);
+    	$final = str_replace($emails,'***',$final);
+    	$final = str_replace($search,'***',$final);
+    	$final = str_replace($tld,'***',$final);
+    	$final = str_replace($others,'***',$final);
+    	$final = preg_replace("#[1-68]([-. /]?[0-9]{2}){4}#",'***',$final);
+    	$final = str_replace(mb_strtolower($name, 'UTF-8'),'***',$final);
+		return $final;                    
+        
+    }
+    
+    public function getName()
+    {
+        return 'twig_encryption';
+    }
+}
